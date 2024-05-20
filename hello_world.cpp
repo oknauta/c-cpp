cd7228fb08aa371489990da8b5cfd5fb57d688ec
oknauta <mauri.ms@outlook.com>
2024-05-20 18:48:45 -0300

    Added `hello_world.cpp`

---
+++ hello_world.cpp
@@ -0,0 +1,20 @@
+/******************************************
+ *        File: hello_world.cpp
+ *     Created: 2024-05-19
+ *    Modified: 2024-05-19
+ *      Author: oknauta
+ * Description: This program prints "Hello, world!" on console in C++. The first thing to do when learning C or C++.
+******************************************/
+
+// The required library to "Hello, world!" be printed
+#include <iostream>
+
+// Function that the compiler will search first.
+int main(int argc, char const *argv[]) 
+{
+    // Printing "Hello, world!"
+
+    std::cout << "Hello, world!\n"; // Prints the "Hello, world"!
+
+    return 0;
+}
