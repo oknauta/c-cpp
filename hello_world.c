7760df6a7f4b42a3775d578d79c68fffdad5a37a
oknauta <mauri.ms@outlook.com>
2024-05-20 18:51:50 -0300

    Added `hello_world.c`

---
+++ hello_world.c
@@ -0,0 +1,20 @@
+/******************************************
+ *        File: hello_world.c
+ *     Created: 2024-05-20
+ *    Modified: 2024-05-20
+ *      Author: oknauta
+ * Description: This program prints "Hello, world!" on console in C The first thing to do when learning C or C++.
+******************************************/
+
+// The required library to "Hello, world!" be printed
+#include <stdio.h>
+
+// Function that the compiler will search first.
+int main(int argc, char const *argv[]) 
+{
+    // Printing "Hello, world!"
+
+    printf("Hello, world!"); // Prints the "Hello, world"!
+
+    return 0;
+}
