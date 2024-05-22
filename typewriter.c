535ceb2f09baf1f2470790aa115eec3c2296f929
oknauta <mauri.ms@outlook.com>
2024-05-21 21:26:51 -0300

    Fixed somethings and updated to better.

--- typewriter.c
+++ typewriter.c
@@ -1,7 +1,7 @@
 /***************************************************************************
  *        File: typewriter.c
  *     Created: 2024-05-20
- *    Modified: 2024-05-20
+ *    Modified: 2024-05-21
  *      Author: oknauta
  * Description: Prints "Hello, world!" character by character.
  *        Note: Created with Microsoft Notepad.
@@ -15,25 +15,23 @@
 /**
  * @brief Prints a string character by character.
  * @param string The text.
- * @param wait The wait to print another character.
+ * @param wait Microseconds to print another character.
  */
-void Typewriter(const char *string, unsigned long int wait);
+const char* Typewriter(const char *string, unsigned long int wait);
 
 int main(int argc, char const *argv[]) 
 {
-    int wait = 100000;
-    Typewriter("Hello...", wait);
-    sleep(1);
-    Typewriter(" world!", wait);
+    Typewriter("Hello, world!", 500000);
     return 0;
 }
 
-void Typewriter(const char *string, unsigned long int wait)
+const char* Typewriter(const char *string, unsigned long int wait)
 {
-                                   // This function is accessing each character from `string`.
-    for(int i = 0; string[i]; ++i) // When a loop is finished, a number is added. So I'm selecting the character from `string`.
+    // This `for` is selecting each character from `string`.
+    for(int i = 0; string[i]; ++i) // When a loop is finished, a number is added. So I'm selecting the next character from `string` to print it.
      {
         printf("%c", string[i]);   // Prints the selected character.
         usleep(wait);              // The wait.
     }
+    return string;
 }
